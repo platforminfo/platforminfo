9f7224fc0d16bd30913de2667d866ea5abed524f
Tejas Raman <tejasraman@icloud.com>
2023-08-25 18:40:49 -0600

    Alpha 1 code

---
+++ platforminfo/__init__.py
@@ -0,0 +1,4 @@
+from importlib import resources
+from .platforminfo import Platform
+
+__version__ = "1.0.0alpha6"
--- platforminfo/platforminfo.py
+++ platforminfo/platforminfo.py
@@ -5,14 +5,32 @@
 import os
 import subprocess
 import sys
-from helper import platforminfolib
 
 if sys.platform == "win32":
     import winreg
 
+def subprocess_postproc(x):
+    return x.stdout.read().strip().decode("utf-8")
+
+
+class PlatformError(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+def Parser(filename, to_be_cut, basestr):
+    with open(filename) as file:
+        x = dict()
+        for line in file:
+            x[line.split("=")[0]] = line.split(to_be_cut)[1]
+
+        version = x[basestr].strip()
+    return version
+
 
 class Platform:
-
     def __init__(self):
         bases = {"win32": "windows", "darwin": "mac", "linux": "linux"}
         self.platform = bases[sys.platform]
@@ -23,10 +41,10 @@ class Platform:
     def kernelVersion(self):
         if self.platform in ["mac", "linux"]:
             kernel = subprocess.Popen(["uname", "-r"], stdout=subprocess.PIPE)
-            return platforminfolib.subprocess_postproc(kernel)
+            return subprocess_postproc(kernel)
 
         elif self.platform == "windows":
-            version = ".".join((platforminfolib.subprocess_postproc(
+            version = ".".join((subprocess_postproc(
                 subprocess.Popen(
                     "wmic os get version /VALUE",
                     shell=True,
@@ -36,7 +54,7 @@ class Platform:
 
     def arch(self):
         if self.platform in ["mac", "linux"]:
-            return platforminfolib.subprocess_postproc(
+            return subprocess_postproc(
                 subprocess.Popen(["uname", "-m"], stdout=subprocess.PIPE))
         else:
             arch = os.environ['PROCESSOR_ARCHITECTURE']
@@ -45,7 +63,7 @@ class Platform:
 
     def buildNumber(self):
         if self.platform == "mac":
-            buildnum = platforminfolib.subprocess_postproc(
+            buildnum = subprocess_postproc(
                 subprocess.Popen("sw_vers -buildVersion",
                                  shell=True,
                                  stdout=subprocess.PIPE))
@@ -60,40 +78,40 @@ class Platform:
             value, type = winreg.QueryValueEx(key, "CurrentBuild")
             return value
         else:
-            raise platforminfolib.PlatformError(
+            raise PlatformError(
                 "PlatformInfo: buildnumber function used on a non-Macintosh/Windows system"
             )
 
     def osVersion(self):
         if self.platform == "linux":
             if os.path.isfile("/etc/os-release"):
-                return platforminfolib.Parser("/etc/os-release", "=",
+                return Parser("/etc/os-release", "=",
                                               "VERSION_ID")
 
             elif os.path.isfile("/usr/lib/os-release"):
-                return platforminfolib.Parser("/usr/lib/os-release", "=",
+                return Parser("/usr/lib/os-release", "=",
                                               "VERSION_ID")
 
             elif os.path.isfile("/etc/lsb-release"):
-                return platforminfolib.Parser("/etc/lsb-release", "=",
+                return Parser("/etc/lsb-release", "=",
                                               "DISTRIB_RELEASE")
 
             elif os.path.isfile("/usr/bin/lsb-release"):
                 version_sp = subprocess.Popen("/usr/bin/lsb_release -r",
                                               shell=True,
                                               stdout=subprocess.PIPE)
-                version = (platforminfolib.subprocess_postproc(
+                version = (subprocess_postproc(
                     version_sp).split(":"))[1]
                 return version
 
         elif self.platform == "mac":
-            return platforminfolib.subprocess_postproc(
+            return subprocess_postproc(
                 subprocess.Popen(["sw_vers -productVersion"],
                                  shell=True,
                                  stdout=subprocess.PIPE))
 
         elif self.platform == "windows":
-            version = (platforminfolib.subprocess_postproc(
+            version = (subprocess_postproc(
                 subprocess.Popen(
                     "wmic os get Name /VALUE",
                     shell=True,
