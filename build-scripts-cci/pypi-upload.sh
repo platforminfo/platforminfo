bdb43bfca2ed0d8466138c0b41a5f6f5981f07bd
Tejas Raman <tejasraman@icloud.com>
2023-09-08 21:14:38 -0600

    Codacy messed my CI workflows up

--- build-scripts-cci/pypi-upload.sh
+++ build-scripts-cci/pypi-upload.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+set -e
 
-set -e PYPI_CONFIG="${HOME}/.pypirc"
+PYPI_CONFIG="${HOME}/.pypirc"
 pip install --upgrade pip
 pip install twine
-echo $"[distutils]\nindex-servers = pypi\n[pypi]" > $PYPI_CONFIG
+echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
 echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
 echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
 twine upload /tmp/dist/*.whl
