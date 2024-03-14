89cf69f685f80470dab425f6f60ec01496fe0811
Tejas Raman <tejasraman@icloud.com>
2024-03-14 10:39:16 -0600

    Fix CLI issues

--- platforminfo/cli.py
+++ platforminfo/cli.py
@@ -1,17 +1,33 @@
 import platforminfo
-import .strings
-
 import sys
 
-VERSION = "1.0.0beta5.post1"
+VERSION = "1.0.0beta5.post2"
 computer = platforminfo.Platform()
 
 
 def picli():
+    help_string = (f"""
+PlatformInfo version {VERSION}
+------------------------------------------------------------
+Arguments:
+------------------------------------------------------------
+-cpuinfo             | Returns all CPU info
+-logical_cores       | Returns number of logical cores
+-physical-cores      | Returns number of physical cores
+-cpuname             | Returns CPU vendoe string (name)
+-desktop-environment | Returns desktop environment (Linux/BSD)
+-os-arch             | Returns OS architecture
+-kernel-version      | Returns kernel version string
+-os-version          | Returns OS version
+-build-number        | Returns OS build number (Windows/macOS)
+-gpuname             | Returns GPU vendor string (name)
+-ram \x1B[3m dataunit \x1B[0m      | Returns ram in specified unit
+    """)
+
     arg = sys.argv[1]
 
     if arg == "-help":
-        print(strings.HelpPrint(VERSION))
+        print(help_string)
     elif arg == "-logical-cores":
         print(computer.cpu_cores("logical"))
     elif arg == "-physical-cores":
