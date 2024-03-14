77f4090a66f10d13d495098e5a317cdc5fe5403b
Tejas Raman <tejasraman@icloud.com>
2024-03-13 20:43:18 -0600

    Poetry tests, add CLI

--- platforminfo/cli.py
+++ platforminfo/cli.py
@@ -1,7 +1,37 @@
 import platforminfo
+import strings
+
 import sys
+
+VERSION = "1.0.0-beta4-post1"
+computer = platforminfo.Platform()
 
 
 def picli():
-    x = platforminfo.Platform()
-    print(x.cpu_cores("logical"))
+    arg = sys.argv[1]
+
+    if arg == "-help":
+        print(strings.HelpPrint(VERSION))
+    elif arg == "-logical-cores":
+        print(computer.cpu_cores("logical"))
+    elif arg == "-physical-cores":
+        print(computer.cpu_cores("physical"))
+    elif arg == "-cpuname":
+        print(computer.cpu_prettyname())
+    elif arg == "-desktop-environment":
+        print(computer.desktop_environment())
+    elif arg == "-os-arch":
+        print(computer.os_architecture())
+    elif arg == "-kernel-version":
+        print(computer.kernel_version())
+    elif arg == "-os-version":
+        print(computer.os_version())
+    elif arg == "-buildnumber":
+        print(computer.build_number())
+    elif arg == "-gpuname":
+        print(computer.gpu_prettyname())
+    elif arg == "-ram":
+        print(computer.ram({sys.argv[2]}))
+
+
+picli()
